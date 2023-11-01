33c5c697c38daf48b94817457a3b826849eecb70
karan-ksrk <karanksrk2000@outlook.com>
2023-11-01 10:00:09 +0530

    test script

--- create_user_script.py
+++ create_user_script.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import django
 
@@ -8,7 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zameendar_backend.settings.dev"
 django.setup()
 
 
-def create_user_from_emails(emails):
+def create_users_from_emails(emails):
     for email in emails:
         user, created = User.objects.get_or_create(username=email, email=email)
         if created:
@@ -18,5 +19,8 @@ def create_user_from_emails(emails):
 
 
 if __name__ == "__main__":
-    email_list = list(map(str.strip, input().split(",")))
-    create_user_from_emails(email_list)
+    if len(sys.argv) < 2:
+        print("Usage: python create_users.py email1 email2 ...")
+    else:
+        email_list = sys.argv[1:]
+        create_users_from_emails(email_list)
