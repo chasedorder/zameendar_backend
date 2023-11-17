bb144e8ff7bd98d718eecafa6f60aa01120cdf72
karan-ksrk <karanksrk2000@outlook.com>
2023-11-17 11:29:15 +0530

    update changes

--- zameendar_backend/celery.py
+++ zameendar_backend/celery.py
@@ -1,18 +1,20 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 
-import os
+# import os
 
-from celery import Celery
-from django.conf import settings
+# from celery import Celery
+# from django.conf import settings
 
-from zameendar_backend.settings import base
+# from zameendar_backend.settings import base
 
-if base.DEBUG:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zameendar_backend.settings.dev")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zameendar_backend.settings.production")
+# if base.DEBUG:
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zameendar_backend.settings.dev")
+# else:
+#     os.environ.setdefault(
+#         "DJANGO_SETTINGS_MODULE", "zameendar_backend.settings.production"
+#     )
 
-app = Celery("zameendar_backend")
+# app = Celery("zameendar_backend")
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# app.config_from_object("django.conf:settings", namespace="CELERY")
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
